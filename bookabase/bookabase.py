b82ccebcec0b8c54c1b8382b76a395126e23d266
vijayakasee-debug <vijayakasee@gmail.com>
2026-09-15 18:53:26 -0700

    Initial commit. Again

--- bookabase/bookabase.py
+++ bookabase/bookabase.py
@@ -1,10 +1,5 @@
 import reflex as rx
-
-class Book(rx.Model, table=True):
-    title: str
-    author: str
-    filepath: str
-    cover_path: str
-    current_chapter: int = 0
-    total_chapters: int = 0
-    progress_percent: float = 0.0
+import io
+from supabase import create_client, Client
+from ebooklib import epub
+from bs4 import BeautifulSoup
